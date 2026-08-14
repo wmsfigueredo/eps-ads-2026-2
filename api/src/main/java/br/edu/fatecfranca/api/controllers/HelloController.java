95f116ea7a8f591fa915561cc19e864ef1237f52
Wellington Miguel <wellingtonmiguel1208@gmail.com>
2026-08-14 13:47:57 +0000

    JavaComSpring

---
+++ api/src/main/java/br/edu/fatecfranca/api/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package br.edu.fatecfranca.api.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+
+   @GetMapping("/")
+   public String hello() {
+       return "API em funcionamento!";
+   }
+
+
+}
