c96006c5cdaf7745dd4033f9fe69bc96f8c7901e
Wellington Miguel <wellingtonmiguel1208@gmail.com>
2026-08-28 13:11:37 +0000

    Criação do projeto beck-end com SpringBoot

--- api/src/main/java/br/edu/fatecfranca/api/controller/HelloController.java
+++ api/src/main/java/br/edu/fatecfranca/api/controller/HelloController.java
@@ -1,10 +1,8 @@
-package br.edu.fatecfranca.api.controllers;
+package br.edu.fatecfranca.api.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @RestController
@@ -15,12 +13,6 @@ public class HelloController {
    public String hello() {
        return "API em funcionamento!";
    }
-   
-   @GetMapping("/users")
-   public String ListUsers() {
-       return "Lista de Usuários";
-   }
-   
 
 
 }
